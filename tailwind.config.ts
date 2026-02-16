c113759cf866a7d70bdaf51f539ed073e3382c1f
ognjeeen <contact.ognjen@gmail.com>
2026-02-16 11:45:59 +0100

    chore: update dependencies and improve TypeScript configuration
    - Updated Next.js to version 16.1.6 and React to version 19.2.4 in package.json
    - Modified resume.pdf to enhance formatting and clarity
    - Adjusted tailwind.config.ts to use updated import syntax for flowbite-react
    - Refined tsconfig.json for better TypeScript support, including target and include/exclude paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
 import type { Config } from 'tailwindcss';
-import flowbite from 'flowbite-react/tailwind';
+import { content, plugin } from 'flowbite-react/tailwind';
 
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    flowbite.content(),
+    content(),
   ],
   theme: {
     extend: {
@@ -22,6 +22,6 @@ const config: Config = {
       Inter: ['Inter'],
     },
   },
-  plugins: [flowbite.plugin()],
+  plugins: [plugin()],
 };
 export default config;
